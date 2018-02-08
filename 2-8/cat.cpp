597723f09e846cdfbbabc7da44f40862f9f37234
Leozhu123 <570727732@qq.com>
2018-02-08 10:09:36 +0800

    18-2-8

--- 2-8/cat.cpp
+++ 2-8/cat.cpp
@@ -1,23 +1,23 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-const int maxn = 10;
-int n, w, num[maxn], a[maxn], maxd, sumb[maxn];
+const long long maxn = 1000;
+long long n, w, num[maxn], a[maxn], maxd, sumb[maxn];
 bool ok, used[maxn];
-bool cmp(int a, int b)
+bool cmp(long long a, long long b)
 {
     if (a > b)
         return true;
     return false;
 }
-bool dfs(int x, int maxd, int rest)
+bool dfs(long long x, long long maxd, long long rest)
 { //使用前x只猫，最多用maxd个缆车，总共还剩rest的空间
     bool ok = false;
     if (x == n + 1)
         return true;
     if (sumb[x] > rest)
         return false;
-    for (int i = 0; i < maxd; i++)
+    for (long long i = 0; i < maxd; i++)
     {
         if (w - a[i] >= num[x])
         {
@@ -31,25 +31,25 @@ bool dfs(int x, int maxd, int rest)
 int main()
 {
     cin >> n >> w;
-    for (int i = 1; i <= n; i++)
+    for (long long i = 1; i <= n; i++)
     {
         cin >> num[i];
     }
     sort(num + 1, num + n + 1, cmp);
-    for (int i = n; i > 0; i--)
+    for (long long i = n; i > 0; i--)
     {
         sumb[i] = sumb[i + 1] + num[i];
     }
-    for (int i = 1; i <= n; i++)
+    for (long long i = 1; i <= n; i++)
     {
-        for (int j = 0; j < maxn; j++)
+        for (long long j = 0; j < maxn; j++)
         {
             a[j] = 0;
-            used[j] = false;
         }
         maxd = i;
         if (dfs(1, maxd, w * maxd))
             break;
     }
     cout << maxd;
+    return 0;
 }
